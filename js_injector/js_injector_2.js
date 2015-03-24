00fad6b6dcc10924213adf24d0cc1bc9bd271d21
andreayaya <andrea@waztech.com>
2015-03-24 11:18:50 -0400

    first commit

---
+++ js_injector/js_injector_2.js
@@ -0,0 +1,7 @@
+(function() {
+  var cx = '001498624467528633936:gt_dejxqt40'; // Insert your own Custom Search engine ID here
+  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+  gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
+      '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
